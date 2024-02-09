e0dc09ea3167485cf45161abd0e57db06a24e9c5
antoinedillen <159175892+antoinedillen@users.noreply.github.com>
2024-02-09 12:49:50 +0100

    Update modifier-titre-et-description.js

--- modifier-titre-et-description.js
+++ modifier-titre-et-description.js
@@ -4,5 +4,5 @@ $(document).ready(function() {
     document.title = "Placealouer";
 
     // Définir la balise meta description
-    $('meta[name="description"]').attr('content', 'Nouvelle description de la page');
+    $('meta[name="description"]').attr('content', 'Placealouer est une application de location de place de parking et de garages entre particuliers. Rentabilisez votre place dès maintenant.');
 });
